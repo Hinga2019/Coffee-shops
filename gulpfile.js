04474b89811c49dc397c4c9dd53244b3ee417ec1
Hinga2019 <hm_inga@hotmail.com>
2022-01-27 01:15:06 -0500

    messing with sass

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+function css( done ) {
+    // compilar sass
+    // pasos: 1 - identificar archivo, 2 - compilarla, 3 - guardar el .css
+
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( postcss([ autoprefixer() ]) )
+        .pipe( dest('build/css') )
+    
+    done();
+}
+
+function dev() {
+    watch('src/scss/app.scss', css );
+}
+
+exports.css = css;
+exports.dev = dev;
